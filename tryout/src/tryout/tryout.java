fc859f0e4dced786e07969d78ee9230782558882
abrown <abrown@LT-013BG-006777.ccea.local>
2018-06-17 16:10:36 +0100

    f2

--- tryout/src/tryout/tryout.java
+++ tryout/src/tryout/tryout.java
@@ -38,7 +38,7 @@ public class tryout {
 	      Date now = new Date();
 	      SimpleDateFormat simpleFormatterx = new SimpleDateFormat("dd/MM/yyyy ");
 	      String x = simpleFormatterx.format(now).trim();
-	      System.out.println("z--"+x+"--z");
+	      System.out.println("zyy--"+x+"--zyy");
 	     
 	}
 
