3911090cdac98d172cffdb5450f847420dde037f
abrown <abrown@LT-013BG-006777.ccea.local>
2018-06-17 16:34:10 +0100

    feature 3 o 2

--- tryout/src/tryout/tryout.java
+++ tryout/src/tryout/tryout.java
@@ -38,7 +38,7 @@ public class tryout {
 	      Date now = new Date();
 	      SimpleDateFormat simpleFormatterx = new SimpleDateFormat("dd/MM/yyyy ");
 	      String x = simpleFormatterx.format(now).trim();
-	      System.out.println("zyyxxfeature2--"+x+"--feature2zyxxy");
+	      System.out.println("zyyxxfeature3--"+x+"--feature3zyxxy");
 	     
 	}
 
